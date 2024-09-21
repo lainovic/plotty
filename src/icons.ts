8ec28fb0929b56734e5591e265b7ffc67cc259e6
Marko Lainović <72805772+markolainovic-tomtom@users.noreply.github.com>
2024-09-21 23:11:51 +0200

    fix icon path changes

--- src/icons.ts
+++ src/icons.ts
@@ -1,21 +1,21 @@
 import { Icon } from "leaflet";
 
 export const originIcon = new Icon({
-  iconUrl: "/icons/origin.svg",
+  iconUrl: "icons/origin.svg",
   iconSize: [64, 64],
   iconAnchor: [32, 36],
   popupAnchor: [0, 0],
 });
 
 export const waypointIcon = new Icon({
-    iconUrl: "/icons/waypoint.svg",
-    iconSize: [32, 32], // size of the icon
-    iconAnchor: [16, 16], // point of the icon which will correspond to marker's location
-    popupAnchor: [0, 0], // point from which the popup should open relative to the iconAnchor
-  });
+  iconUrl: "icons/waypoint.svg",
+  iconSize: [32, 32], // size of the icon
+  iconAnchor: [16, 16], // point of the icon which will correspond to marker's location
+  popupAnchor: [0, 0], // point from which the popup should open relative to the iconAnchor
+});
 
 export const destinationIcon = new Icon({
-  iconUrl: "/icons/destination.svg",
+  iconUrl: "icons/destination.svg",
   iconSize: [64, 64], // size of the icon
   iconAnchor: [32, 42], // point of the icon which will correspond to marker's location
   popupAnchor: [0, 0], // point from which the popup should open relative to the iconAnchor

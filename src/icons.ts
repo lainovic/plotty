a8c51c313f44d97f27200d5ab2353a609937cd01
Marko Lainović <72805772+markolainovic-tomtom@users.noreply.github.com>
2024-11-18 11:52:23 +0100

    add assets and update public icons

--- src/icons.ts
+++ src/icons.ts
@@ -2,22 +2,22 @@ import { Icon } from "leaflet";
 
 export const originIcon = new Icon({
   iconUrl: "icons/origin.png",
-  iconSize: [40, 60], // size of the icon
-  iconAnchor: [20, 60], // point of the icon which will correspond to marker's location
+  iconSize: [40, 52], // size of the icon
+  iconAnchor: [20, 52], // point of the icon which will correspond to marker's location
   popupAnchor: [0, -30], // point from which the popup should open relative to the iconAnchor
 });
 
 export const waypointIcon = new Icon({
   iconUrl: "icons/waypoint.png",
-  iconSize: [40, 60],
-  iconAnchor: [20, 60],
+  iconSize: [40, 52],
+  iconAnchor: [20, 52],
   popupAnchor: [0, -30],
 });
 
 
 export const destinationIcon = new Icon({
   iconUrl: "icons/destination.png",
-  iconSize: [40, 60],
-  iconAnchor: [20, 60],
+  iconSize: [40, 52],
+  iconAnchor: [20, 52],
   popupAnchor: [0, -30],
 });
